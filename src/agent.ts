543f35e6ae382ca31a330c67f32ba8bad8c0b26f
Wlad Paiva <desenvolvedormg@gmail.com>
2023-10-10 21:10:51 -0300

    add chat

---
+++ src/agent.ts
@@ -0,0 +1,70 @@
+import { Message } from "./types";
+
+/**
+ * (In preview) An abstract class for AI agent.
+ *
+ * An agent can communicate with other agents and perform actions.
+ * Different agents can differ in what actions they perform in the `receive` method.
+ */
+export abstract class Agent {
+  private _name: string;
+
+  /**
+   * @param name name of the agent.
+   */
+  constructor(name: string) {
+    this._name = name;
+  }
+
+  /**
+   * Get the name of the agent.
+   * @returns The name of the agent.
+   */
+  get name(): string {
+    return this._name;
+  }
+
+  /**
+   * (Abstract method) Send a message to another agent.
+   * @abstract
+   * @param message The message to send.
+   * @param recipient The recipient agent.
+   * @param requestReply Whether a reply is requested.
+   */
+  abstract send(
+    message: string | Message,
+    recipient: Agent,
+    requestReply?: boolean
+  ): Promise<void>;
+
+  /**
+   * (Abstract async method) Receive a message from another agent.
+   * @abstract
+   * @param message The message received.
+   * @param sender The sender agent.
+   * @param requestReply Whether a reply is requested.
+   */
+  abstract receive(
+    message: string | Message,
+    sender: Agent,
+    requestReply?: boolean
+  ): Promise<void>;
+
+  /**
+   * (Abstract async method) Generate a reply based on the received messages.
+   * @abstract
+   * @param messages A list of messages received.
+   * @param sender The sender agent.
+   * @returns The generated reply. If None, no reply is generated.
+   */
+  abstract generateReply(
+    messages?: Message[],
+    sender?: Agent
+  ): Promise<string | null>;
+
+  /**
+   * (Abstract method) Reset the agent.
+   * @abstract
+   */
+  abstract reset(): void;
+}
---
+++ src/types.ts
@@ -0,0 +1,65 @@
+import type OpenAI from "openai";
+import type { Agent } from "./agent";
+
+/**
+ * OpenAI Chat API message.
+ */
+export type Message = OpenAI.Chat.Completions.ChatCompletionMessageParam;
+export type Role = OpenAI.Chat.Completions.ChatCompletionMessageParam["role"];
+export type Callable = (
+  messages?: Message[],
+  sender?: Agent,
+  config?: LlmConfig
+) => Promise<
+  | {
+      success: false;
+      reply: null;
+    }
+  | {
+      success: true;
+      reply: string;
+    }
+>;
+export type LlmConfig = {
+  // TODO: Add types for this
+};
+
+export type ReplyFunc = {
+  /**
+   * The trigger to activate the reply function.
+   * - If a class is provided, the reply function will be called when the sender is an instance of the class.
+   * - If a string is provided, the reply function will be called when the sender's name matches the string.
+   * - If an agent instance is provided, the reply function will be called when the sender is the agent instance.
+   * - If a callable is provided, the reply function will be called when the callable returns True.
+   * - If a list is provided, the reply function will be called when any of the triggers in the list is activated.
+   * - If None is provided, the reply function will be called only when the sender is None.
+   * Note: Be sure to register `None` as a trigger if you would like to trigger an auto-reply function with non-empty messages and `sender=None`.
+   */
+  trigger: string | Agent | Callable | unknown[];
+  /**
+   * The function takes a recipient agent, a list of messages, a sender agent and a config as input and returns a reply message.
+   */
+  replyFunc: Callable;
+  /**
+   * The position of the reply function in the reply function list.
+   * The function registered later will be checked earlier by default.
+   * To change the order, set the position to a positive integer.
+   */
+  position?: number;
+  /**
+   * The config to be passed to the reply function.
+   * When an agent is reset, the config will be reset to the original value.
+   */
+  config?: LlmConfig;
+  /**
+   * the function to reset the config.
+   * The function returns None.
+   */
+  resetConfig?: Callable;
+};
+
+export type MessageXXX = Message & {
+  // TODO:
+  id: string;
+  createdAt?: Date;
+};
